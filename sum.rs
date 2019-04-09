bedf332c3964b4c604ca1eaf4f1995173b5569c0
Giruba Beulah SE <Giruba@users.noreply.github.com>
2019-04-10 01:07:33 +0530

    Create sum.rs

---
+++ sum.rs
@@ -0,0 +1,7 @@
+fn main() {
+     let value1 = 10;
+     let value2 = 15;
+    
+    let sum = value1 + value2;
+    print!("The sum of the entered two numbers {} and {} is {}", value1, value2, sum);
+}
